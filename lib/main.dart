9b02811892162751234d0c4f0cb1ba293a75afcf
Amier <amierputraa@gmail.com>
2022-04-25 19:55:11 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: prefer_const_constructors
-
-import 'package:crypto/widget/drop_down.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:crypto/services/api_client.dart';
-import 'dart:core';
 
 // ignore: prefer_const_constructors
 void main() => runApp(MyApp());
@@ -26,143 +24,136 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  ApiClient client = ApiClient();
+  TextEditingController textEditingController = TextEditingController();
 
   Color mainColor = Colors.amber;
   Color secondColor = Colors.red;
-  List<String> crypto = [];
-  String from = "";
-  String to = "";
 
-  late double rate = 0.0;
-  String result = "";
+  String name = "", unit = "", desc="";
+  double value = 0.0;
+  var type, base, equal;
 
-  @override
-  void initState() {
-    super.initState();
-    (() async {
-      List<String> list = await client.getExchangeRates();
-      setState(() {
-        crypto = list;
-      });
-    })();
-  }
+  String selectCoin = "Bitcoin";
+  List<String> coinList = [
+    "Bitcoin",
+    "Ether",
+    "Litecoin",
+    "Bitcoin Cash",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: mainColor,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // ignore: sized_box_for_whitespace
-              Container(
-                width: 200,
-                child: const Text(
-                  "Bitcoin Converter",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Expanded(
+        backgroundColor: secondColor,
+        body: SafeArea(
+            child: Padding(
+                padding: const EdgeInsets.all(30.0),
                 child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          labelText: "Input value to convert",
-                          labelStyle: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 16.0,
-                            color: secondColor,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    height: 400.0,
+                    width: 1000,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: mainColor,
+                    ),
+                    child: Column(children: [
+                      Expanded(
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Text("Bitcoin Exchange Rate",
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold)),
+                              const SizedBox(
+                                height: 20.0,
+                              ),
+                              TextField(
+                                controller: textEditingController,
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  labelText: "Input value to convert",
+                                  labelStyle: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 16.0,
+                                    color: secondColor,
+                                  ),
+                                ),
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.start,
+                                keyboardType: TextInputType.number,
+                              ),
+                              DropdownButton(
+                                itemHeight: 60,
+                                value: selectCoin,
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    selectCoin = newValue.toString();
+                                  });
+                                },
+                                items: coinList.map((selectCoin) {
+                                  return DropdownMenuItem(
+                                    child: Text(
+                                      selectCoin,
+                                    ),
+                                    value: selectCoin,
+                                  );
+                                }).toList(),
+                              ),
+                              ElevatedButton(
+                                  onPressed: _loadExchange,
+                                  child: const Text("Load Result")),
+                              Text(
+                                desc,
+                                style: const TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
                         ),
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.start,
-                        keyboardType: TextInputType.number,
                       ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          customDropDown(crypto, from, (val) {
-                            setState(() {
-                              from = val;
-                            });
-                          }),
-                          FloatingActionButton(
-                            onPressed: () {
-                              String temp = from;
-                              setState(() {
-                                from = to;
-                                to = temp;
-                              });
-                            },
-                            child: const Icon(Icons.swap_horiz),
-                            elevation: 0.0,
-                            backgroundColor: secondColor,
-                          ),
-                          customDropDown(crypto, to, (val) {
-                            setState(() {
-                              to = val;
-                            });
-                          }),
-                        ],
-                      ),
-                      SizedBox(height: 50.0),
-                      Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(16.0),
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              "Result",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 24.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              result,
-                              style: TextStyle(
-                                color: secondColor,
-                                fontSize: 36.0,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                    ]),
                   ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                ))));
+  }
+
+  _loadExchange() async {
+    var url = Uri.parse('https://api.coingecko.com/api/v3/exchange_rates');
+    var response = await http.get(url);
+    var rescode = response.statusCode;
+    var cointype;
+
+    if (selectCoin == "Bitcoin") {
+      cointype = "btc";
+    } else if (selectCoin == "Ether") {
+      cointype = "eth";
+    } else if (selectCoin == "Litecoin") {
+      cointype = "ltc";
+    }
+
+    if (rescode == 200) {
+      var jsonData = response.body;
+      var parsedJson = json.decode(jsonData);
+      setState(() {
+        unit = parsedJson['rates']['$cointype']['unit'];
+        value = parsedJson['rates']['$cointype']['value'];
+        type = parsedJson['rates']['$cointype']['type'];
+
+        base = double.parse(textEditingController.text);
+        equal = base*value;
+        desc = "$unit $equal";
+        
+      });
+    } else {
+      throw Exception("Failed to connect to API");
+    }
   }
 }
